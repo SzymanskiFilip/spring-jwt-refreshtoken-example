8466e186b4af7c1cb59ccf22b9a33eff10bc883f
Filip <szymanski.fp@gmail.com>
2023-01-25 15:52:23 +0100

    init

---
+++ src/main/java/eu/filip/jwtrefreshtoken/JwtRefreshtokenApplication.java
@@ -0,0 +1,13 @@
+package eu.filip.jwtrefreshtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRefreshtokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtRefreshtokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/filip/jwtrefreshtoken/JwtRefreshtokenApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.filip.jwtrefreshtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRefreshtokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
